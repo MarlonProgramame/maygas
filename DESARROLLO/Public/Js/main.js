dd34f37816814ecea4dae002bffbe6314c81cc54
sistemas738 <asistente.sistemas1@nuruena.com.co>
2025-08-05 17:04:21 -0500

    se crea pagina principal y se deja toda responsive

--- DESARROLLO/Public/Js/main.js
+++ DESARROLLO/Public/Js/main.js
@@ -8,3 +8,11 @@ window.addEventListener("load", function () {
     contenido.style.display = "block";
   }, 4000); // 5000 milisegundos = 5 segundos
 });
+document.addEventListener('DOMContentLoaded', function () {
+      const hamburger = document.getElementById('hamburger');
+      const navMenu = document.getElementById('nav-menu');
+
+      hamburger.addEventListener('click', function () {
+        navMenu.classList.toggle('active');
+      });
+    });
