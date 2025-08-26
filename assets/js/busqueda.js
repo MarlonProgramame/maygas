d850c8ac4049c60bb0ac3a69c8c94cde989d2ab8
sistemas738 <asistente.sistemas1@nuruena.com.co>
2025-08-26 16:48:29 -0500

    modificaciones pagina

---
+++ assets/js/busqueda.js
@@ -0,0 +1,13 @@
+document.getElementById("searchInput").addEventListener("keyup", function() {
+    let input = this.value.toLowerCase();
+    let products = document.querySelectorAll(".portfolio-item");
+
+    products.forEach(function(product) {
+      let name = product.querySelector(".product-name").textContent.toLowerCase();
+      if (name.includes(input)) {
+        product.style.display = "";
+      } else {
+        product.style.display = "none";
+      }
+    });
+  });
